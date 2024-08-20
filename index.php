56d7096a47dca8d375b18aff012ef7db7eb40b3e
rsaavedravaldivia <rsaavedravaldivia@hccfl.edu>
2024-08-19 22:24:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -47,18 +47,20 @@ function get_curl_response($endpoint, $key, $description)
 $endpoint = 'https://www.napikano.com/api/rcallv1.php';
 $key = '';
 
-$country = 'France';
-$city = 'Paris';
+$country = 'USA';
+$city = 'New York';
 $start = '8/20/2024';
-$end = '8/24/2024';
+$end = '8/27/2024';
 $size = 'solo';
 $age = '20-25';
 $currency = 'USD';
 
 $q = sprintf('Give me an itinerary of two tourist attractions per day in the country of %s, the city of %s, starting on %s and ending in %s. The attractions should be for people traveling %s, age range of %s. 
 
+Ensure that there are two attractions per day for the dates given so if 7 days 14 attractions.
 
-Format the answer as JSON ensure that any quotation marks within the atraction or description fields are properly escaped. Do not add any other text and check that the response is a list of JSON separated by commas and do not add square brackets. The answer should look like this:
+Format the answer as JSON and ensure that any quotation marks within the fields are properly escaped. Do not add any other text and check that the response is a list of JSON separated by commas, do not add square brackets. The answer should look like this:
+
 {"day": "1", 
 "time": "Morning or Afternoon", 
 "date": "00-00-0000", 
@@ -66,7 +68,11 @@ Format the answer as JSON ensure that any quotation marks within the atraction o
 "description": "Attraction Description",
 "duration": "Attraction Duration",
 "price": "Attraction Price in %s"
-}', $country, $city, $start, $end, $size, $age, $currency);
+} 
+Separate each object with only a comma.
+Ensure that the end of the answer does not contain any symbol.
+
+', $country, $city, $start, $end, $size, $age, $currency);
 // add variable to change currency
 // 
 
